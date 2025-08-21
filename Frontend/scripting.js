c16a48b6426d856052abfbfa4371f57c8ec67398
shinkensen <noobywinner587@gmail.com>
2025-08-21 18:24:10 -0500

    cool project!

---
+++ Frontend/scripting.js
@@ -0,0 +1,2 @@
+import {createClient} from 'https://esm.sh/@supabase/supabase-js';
+const sqlthing = createClient("https://vwfwcwvlrssfhqxgqyoz.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3Zndjd3ZscnNzZmhxeGdxeW96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU4MTg0OTcsImV4cCI6MjA3MTM5NDQ5N30.aYVXMVzvLxhBT_o6pwlg75NLVlWppyTqjEy-8-L7mcE")
