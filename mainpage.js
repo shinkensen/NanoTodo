acccb93b1141a6e678762c53ba3132007d1c6305
shinkensen <shinkensen@users.noreply.github.com>
2025-08-31 17:51:05 +0000

    deploy: 253de670263a9136c8b09a467a59f6d1e54a4eed

---
+++ mainpage.js
@@ -0,0 +1,104 @@
+const token=localStorage.getItem('jwt');
+if (!token){
+    window.location.href = '/Frontend/signin.html';
+}
+const names = document.getElementById("hello");
+const todoList = document.getElementById('todo-list');
+const addForm = document.getElementById('submit');
+const addInput = document.getElementById('input-text');
+const logout = document.getElementById('out')
+
+text =  localStorage.getItem('name').split("@")[0];
+names.textContent ="Hello " + text + " 👋";
+async function fetchTodos() {
+    try {
+        const res = await fetch('http://localhost:3000/todos', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        const result = await res.json();
+        if (result.success) {
+            renderTodos(result.todo);
+        } else {
+            console.error(result.error);
+        }
+    } catch (err) {
+        console.error('Error fetching todos:', err);
+    }
+}
+
+function renderTodos(todos) {
+    todoList.innerHTML = '';
+    todos.forEach(todo => {
+        const li = document.createElement('li');
+        const textSpan = document.createElement('span');
+        textSpan.textContent = `${todo.rank}. ${todo.text}`;
+
+        const deleteBtn  = document.createElement('span');
+        deleteBtn.innerHTML = '🗑️';
+        deleteBtn.style.cursor = 'pointer';
+        deleteBtn.marginLeft = '10px';
+        deleteBtn.title = 'Delete Task';
+
+        deleteBtn.addEventListener('click', async() =>{
+            try {
+                const res =await fetch('http://localhost:3000/del',{
+                    method: 'DELETE',
+                    headers:{
+                        'Content-Type' : 'application/json',
+                        'Authorization' :  `Bearer ${token}`
+                    },
+                    body: JSON.stringify({rank: todo.rank})
+                });
+                const result= await res.json();
+                if (result.error) {
+                    console.log(result.error);
+                } else{
+                    fetchTodos();
+                }
+            } catch (err){
+                console.log("Error deleting: ", err);
+            }
+        });
+        li.appendChild(textSpan);
+        li.appendChild(deleteBtn);
+        todoList.appendChild(li);
+    });
+}
+addForm.addEventListener('click', async (e) => {
+    console.log("clicked")
+    e.preventDefault();
+    const text = addInput.value.trim();
+    if (!text) return;
+    try {
+        const res = await fetch('http://localhost:3000/auth', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({ text })
+        });
+        console.log("pushed");
+        const result = await res.json();
+        console.log('Server result:', result);
+        if (result.success) {
+            addInput.value = '';
+            fetchTodos();
+            console.log("success");
+        } else {
+            console.error(result.error);
+        }
+    } catch (err) {
+        console.error('Error adding todo:', err);
+    }
+});
+fetchTodos();
+
+logout.addEventListener('click',() =>{
+    localStorage.removeItem('jwt');
+    localStorage.removeItem('name');
+    window.location.href='/Frontend/signin.html';
+})
---
+++ signup.js
@@ -0,0 +1,69 @@
+import {createClient} from 'https://esm.sh/@supabase/supabase-js';
+const sqlthing = createClient("https://vwfwcwvlrssfhqxgqyoz.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3Zndjd3ZscnNzZmhxeGdxeW96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU4MTg0OTcsImV4cCI6MjA3MTM5NDQ5N30.aYVXMVzvLxhBT_o6pwlg75NLVlWpyTqjEy-8-L7mcE")
+
+
+document.addEventListener('DOMContentLoaded',function(){
+
+
+
+async function signUp(email,password) {
+    let {data,error} = await sqlthing.auth.signUp({email,password});
+    if (error){
+        console.log("sign up error",error);
+    }
+    else{
+        console.log("Signed up:" + data);
+    }
+}
+async function getCurrentUser() {
+    const { data: { user } } = await sqlthing.auth.getUser();
+    if (user) {
+    console.log("Already signed in as:", user.email);
+    } else {
+    console.log("No active session");
+    }
+}
+let error_email=document.getElementById("email_error");
+let error_pass=document.getElementById("pass_error");
+
+
+//sign up stuff
+console.log("hello");
+let email=document.getElementById("email");
+let pass=document.getElementById("password");
+
+const submit=document.getElementById("submitt");
+submit.addEventListener("click", ()=>{
+    email=document.getElementById("email").value;
+    pass=document.getElementById("password").value;
+    console.log(email);
+    console.log(pass);
+    if (email_error(email)&&pass_error(pass)) {
+        signUp(email,pass);
+        error_email.style.border="4px solid black";
+        document.getElementById("error1").style.color="black";
+        error_pass.style.border="4px solid black";
+        document.getElementById("error2").style.color="black";
+        window.location.href="/Frontend/signin.html"
+    }
+    else if (!email_error(email)){
+        error_email.style.border="4px solid red";
+        document.getElementById("error1").style.color="red";
+    }
+    else if(!pass_error(pass)){
+        error_pass.style.border="4px solid orange";
+        document.getElementById("error2").style.color="orange";
+    }
+})
+function email_error(email){
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+function pass_error(password){
+    password=password.toString();
+    if (password.length<6){
+        return false;
+    }
+    return true;
+}
+})
